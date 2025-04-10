563b9a5cfa6f06d28415736d81d60e391e7c3ffa
Red4mber <redamber@proton.me>
2025-04-10 21:02:13 +0200

    fuck subtrees fr all my homies hate subtrees

--- .config/dotfiles/dotfiles.sh
+++ .config/dotfiles/dotfiles.sh
@@ -8,10 +8,10 @@ _dot config --local status.showUntrackedFiles no 2>/dev/null
 
 dot() {
     case $1 in
-        subtree | tree)
-            shift
-            dot_tree "$@"
-            ;;
+        # subtree | tree)
+        #     shift
+        #     dot_tree "$@"
+        #     ;;
         list | ls)
             dot_list
             ;;
@@ -21,17 +21,20 @@ dot() {
     esac
 }
 
-dot_tree() {
-    local prefix=$2
-    local repo="$(dirname $DOTFILES_REPO)/$(basename $prefix)-dotfiles.git"
-    local branch=${3:-main}
-    if [[ -z "$prefix" || -z "$repo" ]]; then
-        echo "Usage: dot tree $1 <prefix> [branch]"
-        return 1
-    fi
-    _dot subtree $1 --prefix="$prefix" "$repo" "$branch" --squash
-}
+# # Naaaaah bro
+# # Fuck subtrees fr fr
 
+# dot_tree() {
+#     local prefix=$2
+#     local repo="$(dirname $DOTFILES_REPO)/$(basename $prefix)-dotfiles.git"
+#     local branch=${3:-main}
+#     if 
+#     if [[ -z "$prefix" || -z "$repo" ]]; then
+#         echo "Usage: dot tree $1 <prefix> [branch]"
+#         return 1
+#     fi
+#     _dot subtree $1 --prefix="$prefix" "$repo" "$branch" 
+# }
 dot_list() {
     (cd "$HOME"
     for i in $(_dot ls-files); do
@@ -40,15 +43,4 @@ dot_list() {
     done
     ) | column -t --separator=¬ -T 2
 }
-# dot-list() {
-#   if [[ "$#" -eq 0 ]]; then
-#     (cd "$HOME"
-#       for i in $(dotls-files); do
-#         echo -n "$(dot -c color.status=always status "$i" -s | sed "s#$i##")"
-#         echo -e "¬/$i¬\e[0;33m$(dot -c color.ui=always log -1 --format="%s" -- "$i")\e[0m"
-#       done
-#     ) | column -t --separator=¬ -T 2
-#   else
-#     dot "$@"
-#   fi
-# }
+
